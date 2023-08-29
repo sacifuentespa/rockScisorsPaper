c384cda367da57ef80c4897ae3a0687d8480892d
Santiago <sacifuentespawk@gmail.com>
2023-08-28 20:21:27 -0500

    Console game finished

--- rockScisorsPaper.js
+++ rockScisorsPaper.js
@@ -13,8 +13,8 @@ function getHumanChoice() {
     let choice;
     let choiceCapitalized;
 
-    while (choiceCapitalized != "Rock" && choiceCapitalized != "Scissors" && choiceCapitalized != "Paper"){
-        
+    while (choiceCapitalized != "Rock" && choiceCapitalized != "Scissors" && choiceCapitalized != "Paper") {
+
         choice = prompt("Choose quickly only Rock, Scissors or Paper!");
         choiceCapitalized = choice[0].toUpperCase();
 
@@ -22,18 +22,51 @@ function getHumanChoice() {
             choiceCapitalized = choiceCapitalized + choice[i].toLowerCase();
         }
     }
-    
+
     return choiceCapitalized;
 }
 
-function checkRound(computer,human){
+function checkRound(computer, human) {
     let computerChoice = computer;
     let humanChoice = human;
-    if(computerChoice==humanChoice){
-        return "it's a tie!"
-    }else if(computerChoice!=humanChoice){
-        return "terminator"
+    console.log(computerChoice)
+    console.log(humanChoice)
+    if (computerChoice == humanChoice) {
+        return "It's a tie!";
+    } else if ((computerChoice == "Rock" && humanChoice == "Scissors") ||
+        (computerChoice == "Scissors" && humanChoice == "Paper") ||
+        (computerChoice == "Paper" && humanChoice == "Rock")) {
+        return "Computer";
+    } else {
+        return "Human";
     }
 }
 
-console.log(checkRound(getComputerChoice(),getHumanChoice()));
+function game() {
+    let computerScore = 0;
+    let humanScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+
+        let roundResult = checkRound(getComputerChoice(), getHumanChoice());
+        console.log("This round goes for: ", roundResult)
+        if (roundResult == "Computer") {
+            computerScore += 1;
+        } else if (roundResult == "Human") {
+            humanScore += 1;
+        }
+    }
+    
+    if (computerScore > humanScore){
+        console.log("Computer wins!");
+        return 0;
+    }else if (computerScore < humanScore){
+        console.log("Human wins!");
+        return 0;
+    }else {
+        console.log("It was a tie!")
+    }
+    
+}
+
+game();
